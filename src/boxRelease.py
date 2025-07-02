8c6ac19ea77ebb162eea4eba36dc1df186460b7b
boxlegs <tobyturns133@gmail.com>
2025-07-02 22:33:40 +1000

    Added some token validation and error notifs

--- src/boxRelease.py
+++ src/boxRelease.py
@@ -9,30 +9,45 @@ import argparse
 
 load_dotenv()
 
-ACCESS_TOKEN = os.getenv("HTB_ACCESS_TOKEN")
-TIMEZONE = os.getenv("TIMEZONE")
+def main():
+    
+    # Parse args and env vars
+    ACCESS_TOKEN = os.getenv("HTB_ACCESS_TOKEN")
+    TIMEZONE = os.getenv("TIMEZONE")
 
-print("Welcome to the Hack The Box Machine Notifier!")
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-u', '--url', help="ntfy server URL. If not supplied, https://ntfy.sh is used by default.")
+    parser.add_argument('-t', '--topic', help="ntfy server topic slug. If not supplied, 'newbox' is used by default.")
+    args = parser.parse_args()
 
-client = HTBClient(app_token=ACCESS_TOKEN)
-data = client.do_request("season/machines")
-machines = data['data']
+    ntfy_url = args.url.strip() if args.url else "https://ntfy.sh/"
+    ntfy_topic = args.topic.strip() if args.topic else "newbox" 
+    ntfy_client = NtfyClient(ntfy_topic, ntfy_url)
 
-# Just loop through
-
-parser = argparse.ArgumentParser()
-parser.add_argument('-u', '--url', help="ntfy server URL. If not supplied, https://ntfy.sh is used by default.")
-args = parser.parse_args()
-
-ntfy_url = args.url.strip() if args.url else "https://ntfy.sh/"
-
-
-for machine in machines:
-    if not machine['unknown']:
-        release_time = datetime.datetime.fromisoformat(machine['release_time'])
-        now = datetime.datetime.now(pytz.timezone(TIMEZONE))
+    # Check token exists and is valid
+    if ACCESS_TOKEN is None:
+        print("Please set the ACCESS_TOKEN environment variable.")
+        exit(1)
         
-        if abs(now - release_time) <= datetime.timedelta(hours=24) and machine['is_released']:
-            client = NtfyClient('newbox', ntfy_url)
-            client.send(f"\nA new machine has been released on HTB.\n\n**Name:** {machine['name']}\n**Difficulty:** {machine['difficulty_text']}\n**OS:** {machine['os']}\n\n Get to it!", f"New Machine {machine['name']} Just Dropped!", priority=client.MessagePriority.HIGH, tags=['package'], format_as_markdown=True)
+    try:   
+        htb_client = HTBClient(app_token=ACCESS_TOKEN)
+        data = htb_client.do_request("season/machines")
+    except Exception as e:
+        print(ntfy_client.send(f"Your HTB access token has expired, or API endpoints have changed.", "FATAL ERROR", tags=["warning"], priority=ntfy_client.MessagePriority.HIGH))
+        print("Error: ACCESS_TOKEN has likely expired.")
+        exit(1)
+        
+    # Iterate through active machiens
+    machines = data['data']
+    for machine in machines:
+        if not machine['unknown']:
+            release_time = datetime.datetime.fromisoformat(machine['release_time'])
+            now = datetime.datetime.now(pytz.timezone(TIMEZONE))
+            
+            if abs(now - release_time) <= datetime.timedelta(hours=24) and machine['is_released']:
+                ntfy_client = NtfyClient('newbox', ntfy_url)
+                ntfy_client.send(f"\nA new machine has been released on HTB.\n\n**Name:** {machine['name']}\n**Difficulty:** {machine['difficulty_text']}\n**OS:** {machine['os']}\n\n Get to it!", f"New Machine {machine['name']} Just Dropped!", priority=ntfy_client.MessagePriority.HIGH, tags=['package'], format_as_markdown=True)
 
+
+if __name__ == "__main__":
+    main()
